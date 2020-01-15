76e4dcb9c065aa03b0b85b3a718745c0e25d6abe
Karen Hakobyan <karenhakobyan2001@gmail.com>
2020-01-15 12:26:42 +0400

    Added bulder for Student

--- src/University/Setting.java
+++ src/University/Setting.java
@@ -12,26 +12,80 @@ public class Setting {
     protected static Faculty Translation = new Faculty("Translation",TranslationSub);
     protected static Faculty Engineering = new Faculty("Engineering",EngineeringSub);
 
-    protected static Student FirstStudent = new Student("FirstStudent",CyberSecurity,1);
-    protected static Student SecondStudent = new Student("SecondStudent",CyberSecurity,1);
-    protected static Student ThirdStudent = new Student("ThirdStudent",CyberSecurity,1);
-    protected static Student FourthStudent = new Student("FourthStudent",CyberSecurity,2);
-    protected static Student FifthStudent = new Student("FifthStudent",CyberSecurity,2);
-    protected static Student SixthStudent = new Student("SixthStudent",CyberSecurity,2);
+    protected static Student FirstStudent = new Student.StudentBuilder("FirstStudent")
+            .faculty(CyberSecurity)
+            .group(1)
+            .build();
+    protected static Student SecondStudent = new Student.StudentBuilder("SecondStudent")
+            .faculty(CyberSecurity)
+            .group(1)
+            .build();
+    protected static Student ThirdStudent = new Student.StudentBuilder("ThirdStudent")
+            .faculty(CyberSecurity)
+            .group(1)
+            .build();
+    protected static Student FourthStudent = new Student.StudentBuilder("FourthStudent")
+            .faculty(CyberSecurity)
+            .group(2)
+            .build();
+    protected static Student FifthStudent = new Student.StudentBuilder("FifthStudent")
+            .faculty(CyberSecurity)
+            .group(2)
+            .build();
+    protected static Student SixthStudent = new Student.StudentBuilder("SixthStudent")
+            .faculty(CyberSecurity)
+            .group(2)
+            .build();
 
-    protected static Student SeventhStudent = new Student("SeventhStudent",Translation,3);
-    protected static Student EighthStudent = new Student("EighthStudent",Translation,3);
-    protected static Student NinthStudent = new Student("NinthStudent",Translation,3);
-    protected static Student TenthStudent = new Student("TenthStudent",Translation,4);
-    protected static Student EleventhStudent = new Student("EleventhStudent",Translation,4);
-    protected static Student TwelfthStudent = new Student("TwelfthStudent",Translation,4);
+    protected static Student SeventhStudent = new Student.StudentBuilder("SeventhStudent")
+            .faculty(Translation)
+            .group(3)
+            .build();
+    protected static Student EighthStudent = new Student.StudentBuilder("EighthStudent")
+            .faculty(Translation)
+            .group(3)
+            .build();
+    protected static Student NinthStudent = new Student.StudentBuilder("NinthStudent")
+            .faculty(Translation)
+            .group(3)
+            .build();
+    protected static Student TenthStudent = new Student.StudentBuilder("TenthStudent")
+            .faculty(Translation)
+            .group(4)
+            .build();
+    protected static Student EleventhStudent = new Student.StudentBuilder("EleventhStudent")
+            .faculty(Translation)
+            .group(4)
+            .build();
+    protected static Student TwelfthStudent = new Student.StudentBuilder("TwelfthStudent")
+            .faculty(Translation)
+            .group(4)
+            .build();
 
-    protected static Student ThirteenthStudent = new Student("ThirteenthStudent",Engineering,5);
-    protected static Student FourteenthStudent = new Student("FourteenthStudent",Engineering,5);
-    protected static Student FifteenthStudent = new Student("FifteenthStudent",Engineering,5);
-    protected static Student SixteenthStudent = new Student("SixteenthStudent",Engineering,6);
-    protected static Student SeventeenthStudent = new Student("SeventeenthStudent",Engineering,6);
-    protected static Student EighteenthStudent = new Student("EighteenthStudent",Engineering,6);
+    protected static Student ThirteenthStudent = new Student.StudentBuilder("ThirteenthStudent")
+            .faculty(Engineering)
+            .group(5)
+            .build();
+    protected static Student FourteenthStudent = new Student.StudentBuilder("FourteenthStudent")
+            .faculty(Engineering)
+            .group(5)
+            .build();
+    protected static Student FifteenthStudent = new Student.StudentBuilder("FifteenthStudent")
+            .faculty(Engineering)
+            .group(5)
+            .build();
+    protected static Student SixteenthStudent = new Student.StudentBuilder("SixteenthStudent")
+            .faculty(Engineering)
+            .group(6)
+            .build();
+    protected static Student SeventeenthStudent = new Student.StudentBuilder("SeventeenthStudent")
+            .faculty(Engineering)
+            .group(6)
+            .build();
+    protected static Student EighteenthStudent = new Student.StudentBuilder("EighteenthStudent")
+            .faculty(Engineering)
+            .group(6)
+            .build();
 
     protected static float AverageCounter(int[] marks){
         int sum = 0;
--- src/University/Student.java
+++ src/University/Student.java
@@ -4,12 +4,37 @@ public class Student extends Group {
     protected String StudentName;
     protected int[] Marks;
 
-    protected Student(String StudentName,Faculty faculty,int GroupNumber) {
-            this.StudentName = StudentName;
-            this.FacultyName = faculty.FacultyName;
-            this.GroupNumber = GroupNumber;
-            this.Subjects = faculty.Subjects;
+    protected Student(StudentBuilder builder) {
+            StudentName = builder.StudentName;
+            FacultyName = builder.faculty.FacultyName;
+            GroupNumber = builder.GroupNumber;
+            Subjects = builder.faculty.Subjects;
 
     }
 
+    protected static class StudentBuilder {
+        protected String StudentName;
+        Faculty faculty;
+        int GroupNumber;
+
+        protected StudentBuilder(String Name) {
+            this.StudentName = Name;
+        }
+
+        protected StudentBuilder faculty(Faculty faculty) {
+            this.faculty = faculty;
+            return this;
+        }
+
+        protected StudentBuilder group(int GroupNumber) {
+            this.GroupNumber = GroupNumber;
+            return this;
+        }
+
+        protected Student build(){
+            Student s = new Student(this);
+            return s;
+        }
+
+    }
 }
